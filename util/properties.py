6947196937c2a34ece08525647f916b0d1b90a47
ultimus2935 <gyanthammaiah2935@gmail.com>
2024-11-26 08:08:14 +0530

    Update properties.py
    - Changed default window dimensions to 1280x720
    - Changed default system to be loaded

--- util/properties.py
+++ util/properties.py
@@ -2,9 +2,9 @@ import numpy as np
 
 DEBUG = False
 
-SCREEN_SIZE = SCREEN_WIDTH, SCREEN_HEIGHT = 800, 600
-ORIGIN = np.array([SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2])
+SCREEN_SIZE = SCREEN_WIDTH, SCREEN_HEIGHT = 1280, 720
+ORIGIN = np.array([SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2])
 
-SYSTEM_FILE_PATH = "systems/two_body_example.csv"
+SYSTEM_FILE_PATH = "systems/sun_earth_moon.csv"
 
 dt = 0.01
