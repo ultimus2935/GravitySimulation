7a135e478a8b3138432e76b93873f7d4f13b9b4e
ultimus2935 <gyanthammaiah2935@gmail.com>
2024-11-26 08:12:51 +0530

    Added fullscreen, zoom, and pause functionality
    - Added fullscreen and zoom functionality.
    - Simulation can be paused using P key.
    - Temporarily disabled electrostatic interaction.
    - Removed need for ORIGIN vector by translating the camera.
    - Removed FPS display
    - Refactored imports and some variable names.
    - Added color as a parameter for Body class.
    - Fixed minor issue with loading system file

--- main.py
+++ main.py
@@ -4,74 +4,89 @@ import math
 import csv
 
 import numpy as np
-from numpy import *
+from numpy import array, zeros, pi
 from numpy.linalg import norm
 
 import pyglet
-from pyglet import *
+from pyglet import shapes, graphics, canvas
+from pyglet.window import key, Window
 
 sys.path.append(os.path.dirname(os.path.dirname(__file__) + '/../util'))
 from util.properties import *
 from util.constants import *
 
-win = window.Window(*ORIGIN, caption = 'Gravity Simulator', vsync = False)
+win = Window(
+    *SCREEN_SIZE, 
+    caption = 'Gravity Simulator', 
+    vsync = False, resizable = False
+)
 
-if DEBUG: fps = window.FPSDisplay(win)
+if FULLSCREEN:
+    screen = canvas.get_display().get_screens()[0]
+    win.set_size(screen.width, screen.height)
+    win.set_fullscreen(True)
+    
+ORIGIN = array([win.width // 2, win.height // 2])
+    
+win.view = win.view.translate((*ORIGIN, 1))
+win.view = win.view.scale((*ZOOM_FACTOR, 1))
 
 kineticBodies = graphics.Batch()
 kineticBodies.list = []
 
 class Body():
-    def __init__(self, name = '',
-                mass_density = 0, charge_density = 0, radius = 0, 
-                position = np.zeros(2), velocity = np.zeros(2),
-                batch = None
-                ):
+    def __init__(
+        self, name = '',
+        mass_density = 0, charge_density = 0, radius = 0, 
+        position = zeros(2), velocity = zeros(2),
+        color =  (255, 255, 255), batch = None
+        ):
+        
         self.name = name
         
-        self.pos = position
-        self.vel = velocity
+        self.position = position
+        self.velocity = velocity
         
         self.mass = mass_density * pi * radius**2
         self.charge = charge_density * pi * radius**2
         
         self.sprite = shapes.Circle(
-            *(ORIGIN + position), radius = radius,
-            batch = batch
+            *position, radius = radius,
+            batch = batch, color = color + (255,)
         )
         
     def gravity(self, bodies):
-        acc = np.zeros(2)
-        if self.mass == 0: return acc
+        acceleration = zeros(2)
+        if self.mass == 0: return acceleration
         
         for body in bodies:
             if body == self or body.mass == 0: continue
             
-            r = body.pos - self.pos
-            acc += G * body.mass * (r / np.linalg.norm(r)**3)
+            r = body.position - self.position
+            acceleration += G * body.mass * (r / norm(r)**3)
         
-        return acc
+        return acceleration
 
     def electrostatic(self, bodies):
-        acc = np.zeros(2)
-        if self.mass == 0: return acc
+        acceleration = zeros(2)
+        if self.mass == 0: return acceleration
         
         for body in bodies:
             if body == self or body.charge == 0: continue
             
-            r = body.pos - self.pos
-            acc += (k * body.charge * self.charge / self.mass) * (r / np.linalg.norm(r)**3)
+            r = body.position - self.position
+            acceleration += (k * body.charge * self.charge / self.mass) * (r / norm(r)**3)
                 
-        return acc
+        return acceleration
             
     def update(self, bodies, dt):
-        acc = self.gravity(bodies)
-        acc += self.electrostatic(bodies)
+        acceleration = self.gravity(bodies)
+        # acceleration += self.electrostatic(bodies) # Uncomment to enable electrostaic forces
         
-        self.vel += acc * dt
-        self.pos += self.vel * dt
+        self.velocity += acceleration * dt
+        self.position += self.velocity * dt
         
-        self.sprite.position = ORIGIN + self.pos
+        self.sprite.position = self.position
 
 def loadBodies(path):
     fp = open(SYSTEM_FILE_PATH, 'r')
@@ -94,14 +109,20 @@ def loadBodies(path):
 def on_draw():
     win.clear()
     
-    if DEBUG: fps.draw()
     kineticBodies.draw()
 
+@win.event
+def on_key_press(symbol, modifiers):
+    global RUNNING
+    
+    if symbol == key.P: RUNNING = not RUNNING
+
 def update(dt):
-    for body in kineticBodies.list: 
-        body.update(kineticBodies.list, dt)
+    if RUNNING:
+        for body in kineticBodies.list: 
+            body.update(kineticBodies.list, dt)
 
 if __name__ == '__main__':
-    loadBodies('systems/two_body.csv')
+    loadBodies(SYSTEM_FILE_PATH)
     pyglet.clock.schedule_interval(update, dt)
-    app.run()
+    pyglet.app.run()
--- util/properties.py
+++ util/properties.py
@@ -1,9 +1,12 @@
 import numpy as np
 
-DEBUG = False
+RUNNING = False
 
 SCREEN_SIZE = SCREEN_WIDTH, SCREEN_HEIGHT = 1280, 720
 ORIGIN = np.array([SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2])
+
+FULLSCREEN = True
+ZOOM_FACTOR = np.array([2, 2])
 
 SYSTEM_FILE_PATH = "systems/sun_earth_moon.csv"
 
